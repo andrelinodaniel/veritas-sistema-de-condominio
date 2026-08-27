15c63ee77463c6db6b6998cb40b9431b4ace8118
Andrelino Daniel Filho <107147363+andrelinodaniel@users.noreply.github.com>
2026-08-27 16:21:46 -0400

    feat(api): cria os serializers e views para usuario e chamado

---
+++ condominio/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Usuario, Chamado
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id','username','cpf','telefone','is_sindico','endereco']
+        depth = 1
+
+class ChamadoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chamado
+        fields = ['id', 'titulo', 'descricao', 'status', 'autor', 'created_at']
+        depth = 1
--- condominio/views.py
+++ condominio/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Usuario, Chamado
+from .serializers import UsuarioSerializer, ChamadoSerializer
 
-# Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [IsAuthenticated]
+
+class ChamadoViewSet(viewsets.ModelViewSet):
+    queryset = Chamado.objects.all()
+    serializer_class = ChamadoSerializer
+    permission_classes = [IsAuthenticated]
